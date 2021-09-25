14e6f987fd8cf07248906a53d8ae6ffa41eb6444
Hiroyuki Tanaka <aflc0x@gmail.com>
2021-09-25 09:34:50 +0900

    ♻️  refactor some configs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 OPTION_BACKEND_STORE_URI=""
-if [ -n "${DB_ENABLED}" ]; then
+if [ -n "${DB_HOST}" ]; then
     _DB_PASS="${DB_PASSWORD}"
     if [ -n "${DB_PASSWORD_FILE}" ]; then
         _DB_PASS="`cat ${DB_PASSWORD_FILE}`"
@@ -9,9 +9,4 @@ if [ -n "${DB_ENABLED}" ]; then
     OPTION_BACKEND_STORE_URI="--backend-store-uri='postgresql://${DB_USER}:${_DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}'"
 fi
 
-OPTION_PORT="--port=80"
-if [ -n "${MLFLOW_PORT}" ]; then
-    OPTION_PORT="--port=${MLFLOW_PORT}"
-fi
-
-exec mlflow server --host 0.0.0.0 $OPTION_PORT $OPTION_BACKEND_STORE_URI "$@"
+exec mlflow server --host 0.0.0.0 $OPTION_BACKEND_STORE_URI "$@"
