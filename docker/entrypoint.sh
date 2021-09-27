3a91411cfbcf41d88e39ba2c97c0b71f42ae953b
Hiroyuki Tanaka <aflc0x@gmail.com>
2021-09-27 10:51:12 +0900

    🐛  local not in a function

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,8 @@
 OPTION_BACKEND_STORE_URI=""
 if [ -n "${DB_HOST}" ]; then
     if [ -n "${DB_PASSWORD_FILE}" ]; then
-        local pass=`cat ${DB_PASSWORD_FILE}`
-        echo "${DB_HOST}:${DB_PORT}:${DB_DATABASE}:${DB_USER}:${pass}" > ~/.pgpass
+        _pass=`cat ${DB_PASSWORD_FILE}`
+        echo "${DB_HOST}:${DB_PORT}:${DB_DATABASE}:${DB_USER}:${_pass}" > ~/.pgpass
         chmod 600 ~/.pgpass
         OPTION_BACKEND_STORE_URI="--backend-store-uri=postgresql://${DB_USER}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
     elif [ -n "${DB_PASSWORD}"]; then
